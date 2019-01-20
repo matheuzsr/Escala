34068e7c8c15e36c1e11bdaa4cdbe2bd7f547abf
matheuzsr <43797621+matheuzsr@users.noreply.github.com>
2019-01-20 00:28:28 -0200

    Ajustado a tela aosamsung j7

--- app/src/main/java/com/example/janaina/escalahorarios/CalendarActivity.java
+++ app/src/main/java/com/example/janaina/escalahorarios/CalendarActivity.java
@@ -2,12 +2,29 @@ package com.example.janaina.escalahorarios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView myData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        //calendarView = (CalendarView) findViewById(R.id.calendarView);
+            //calendarView.set
+
+
+        //Calendar c = Calendar.getInstance();
+        //c.set(2019,1,17);
+        //long ordDia;
+        //ordDia = c.getTimeInMillis()/86400000;
+
     }
 }
